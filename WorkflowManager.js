ff94c7f9fdb7b8f2a23f6b4af3525e17c32c3a35
Elson Oliveira <elson.oliveira@fdte.io>
2021-03-31 14:57:44 -0300

    Alteração dos tópicos inscritos; Utilização de Switch Case; Console.log para ajudar no debug; Utilização de Focus, Create e Remove

--- WorkflowManager.js
+++ WorkflowManager.js
@@ -1,19 +1,24 @@
 import { useState, useEffect } from "react";
 import { Client } from "paho-mqtt";
-import {removeActivityManager,addActivityManager} from "./workflowManager.slice";
-
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import {
+  removeActivityManager,
+  addActivityManager,
+  setCurrentActivityManager,
+  setFocusProcess,
+} from "./workflowManager.slice";
+import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 
 export function WorkflowManager({
   mqttConfig = {
     host: "broker.hivemq.com",
     port: 8000,
-    clientId: 'fbtaskr896967579608'
+    clientId: "fbtaskr896967579608",
   },
   children,
   ...otherProps
 }) {
-  
   const dispatch = useDispatch();
   const [mqttClient, setMqttClient] = useState(null);
   const [badConnFlag, setBadConnFlag] = useState(null);
@@ -43,8 +48,8 @@ export function WorkflowManager({
       function onConnect() {
         // Once a connection has been made, make a subscription and send a message.
         console.log("Connected");
-        client.subscribe("/session/#");
-        client.subscribe("/actor/#");
+        client.subscribe("/session/+/am/#");
+        client.subscribe("/actor/+/am/#");
       }
 
       // called when the client loses its connection
@@ -56,18 +61,76 @@ export function WorkflowManager({
       }
 
       // called when a message arrives
-      function onMessageArrived(message) {
-        let processId;
+      async function onMessageArrived(message) {
+        console.log("Receveid message", message.topic);
         const activityManager = JSON.parse(message.payloadString);
+        const topic = message.topic;
 
-        if(message.topic.includes('/session/')) {
-          dispatch(addActivityManager({ activityManager }));
-        } else if (message.topic.includes('/actor/')) {
-          dispatch(addActivityManager({ activityManager }));
-        } else if(message.topic.includes('/activity/')) {
-          dispatch(removeActivityManager({activityManagerId: activityManager.id}))
-        } else {
-          // console.log('MENSAGEM ELSE', message.topic)
+        const session_id = await AsyncStorage.getItem("@session_id");
+        const actor_id = await AsyncStorage.getItem("@actor_id");
+
+        console.log("ASYNC session_id", session_id);
+        console.log("ASYNC actor_id", actor_id);
+
+        let action = "";
+        let messageConsole = "";
+
+        switch (topic) {
+          case `/session/${session_id}/am/create`:
+          case `/actor/${actor_id}/am/create`:
+            action = "(A action é " + activityManager.props.action + ").";
+            messageConsole = `\x1b[31m${"\n\tEntrou no CREATE."}\x1b[36m${action}\x1b[0m`;
+            messageConsole += `\n\x1b[36m\t- Activity Manager adicionado na fila. (ID: ${activityManager.id})\x1b[0m`;
+            console.log(messageConsole);
+
+            dispatch(addActivityManager({ activityManager }));
+            break;
+
+          case `/session/${session_id}/am/remove`:
+          case `/actor/${actor_id}/am/remove`:
+            messageConsole = `\x1b[31m${"\n\tEntrou no REMOVE."}.`;
+            messageConsole += `\n\x1b[36m\t- Activity Manager removido na fila. (ID: ${activityManager.activity_manager_id})\x1b[0m`;
+            console.log(messageConsole);
+
+            dispatch(
+              removeActivityManager({
+                activityManagerId: activityManager.activity_manager_id,
+              })
+            );
+            break;
+
+          case `/session/${session_id}/am/focus`:
+          case `/actor/${actor_id}/am/focus`:
+            action = "(A action é " + activityManager?.props?.action + ").";
+            messageConsole = `\x1b[31m${"\n\tEntrou no FOCUS da Atividade."}\x1b[36m${action}\x1b[0m`;
+            messageConsole += `\n\x1b[36m\t- Foco do processo setado para o da atividade. (${activityManager.process_id})`;
+            messageConsole += `\n\x1b[36m\t- Atividade atual setado para o id da atividade. (${activityManager.id})\x1b[0m`;
+            console.log(messageConsole);
+
+            dispatch(
+              setFocusProcess({ processId: activityManager.process_id })
+            );
+            dispatch(
+              setCurrentActivityManager({
+                activityManagerId: activityManager.id,
+              })
+            );
+            break;
+
+          case `/session/${session_id}/process/focus`:
+          case `/actor/${actor_id}/process/focus`:
+            action = "(A action é " + activityManager?.props?.action + ").";
+            messageConsole = `\x1b[31m${"\n\tEntrou no FOCUS do Processo."}\x1b[36m${action}\x1b[0m`;
+            messageConsole += `\n\x1b[36m\t- Foco do processo setado para o da atividade. (${activityManager.process_id})\x1b[0m`;
+            console.log(messageConsole);
+
+            dispatch(
+              setFocusProcess({ processId: activityManager.process_id })
+            );
+            break;
+
+          default:
+            console.log("\n\x1b[31mTÓPICO NÃO ENCONTRADO\x1b[0m\n");
         }
       }
 
@@ -80,7 +143,7 @@ export function WorkflowManager({
         try {
           mqttClient.disconnect();
         } catch (e) {
-          console.log('DISCONECTED', e);
+          console.log("DISCONECTED", e);
         } finally {
           setMqttClient(null);
           console.log("Disconnected");
