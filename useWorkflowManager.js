923add915c09e44d3efd14034cb6ce055f4ed12e
Elson Oliveira <elson.oliveira@fdte.io>
2021-04-14 14:36:50 -0300

    New functions created

--- useWorkflowManager.js
+++ useWorkflowManager.js
@@ -1,19 +1,50 @@
 import { useDispatch, useSelector } from "react-redux";
 
 import {
+  getAvailableActivityManagerForProcessAsync,
   selectCurrentActivityManager,
   startWorkflowAsync,
   submitActivityToActivityManagerAsync,
+  updateDefaultProcess,
 } from "./workflowManager.slice";
+
+import { loginAction, updateState as updateLogin } from "./login.slice";
 
 export function useWorkflowManager() {
   const dispatch = useDispatch();
   const currentActivity = useSelector(selectCurrentActivityManager);
 
   /**
+   * Make a Anonymous Login
+   * @param {String} URL The URL to request an anonymous Login
+   */
+  function anonymousLogin(URL) {
+    dispatch(loginAction.getAnonymousToken(URL));
+  }
+
+  function getAvailableActivityByProcessId(processId) {
+    dispatch(getAvailableActivityManagerForProcessAsync(processId));
+  }
+
+  /**
+   * Set the default process
+   * @param {String} processId It represents the default process' Id
+   */
+  function setDefaultProcess(processId) {
+    dispatch(updateDefaultProcess({ defaultProcess: processId }));
+  }
+
+  /**
+   * Set login data
+   * @param {Object} login Represents login info (eg.: actor_id)
+   */
+  function setLogin(login) {
+    dispatch(updateLogin(login));
+  }
+
+  /**
    * Start an workflow
    * @param {String} workflowName Represents the workflow's name
-   * @param {String} [body = {}] JSON to send to start the workflow
    */
   function startWorkflow(workflowName, body = {}) {
     dispatch(startWorkflowAsync(workflowName, body));
@@ -28,5 +59,13 @@ export function useWorkflowManager() {
     dispatch(submitActivityToActivityManagerAsync(activityId, payload));
   }
 
-  return { currentActivity, startWorkflow, submitActivity };
+  return {
+    anonymousLogin,
+    currentActivity,
+    getAvailableActivityByProcessId,
+    setDefaultProcess,
+    setLogin,
+    startWorkflow,
+    submitActivity,
+  };
 }
